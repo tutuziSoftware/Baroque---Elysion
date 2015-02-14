5e9e34bfc4e84f12b78e30bb4c9271e387a76897
ゆきかご <p.u.r.u.h.i.m.e@gmail.com>
2015-02-15 01:53:14 +0900

    API疎通完了までいけた

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var baroque = angular.module('Baroque', []);
 
 baroque.controller("main", ['$scope', '$http', function($scope, $http){
-    var api = new Twitter4FxOS($http);
-    api.executeOAuth().then(function(){
-        api.getHomeTimeline(function(){
-            console.log("api.getHomeTimeline");
-        });
-    }).catch(function(){
-        $scope.accessToken = false;
+    var api = new Twitter4FxOS($http, function(call){
+        if(call == Twitter4FxOS.prototype.NG){
+            api.executeOAuth().then(function(){
+                location.reload();
+            }).catch(function(){
+                location.reload();
+            });
+        }else{
+            $scope.isApi = true;
+        }
     });
+
+    $scope.fetchAccessToken = function(){
+        api.fetchAccessToken($scope.pin).then(function(){
+            console.log("API standing by");
+        }).catch(function(){
+            console.log("");
+        });
+    };
 }]);
--- twitter4js.js
+++ twitter4js.js
@@ -1,13 +1,18 @@
 var Twitter4FxOS = (function(){
-    function Twitter4FxOS($http){
+    function Twitter4FxOS($http, callback){
         this._storage = new Storage("accessToken");
 
         this._storage.getItem().then(function(accessToken){
             this._network = new Network($http, accessToken);
+            callback(Twitter4FxOS.prototype.OK);
         }.bind(this)).catch(function(){
             this._network = new Network($http);
+            callback(Twitter4FxOS.prototype.NG);
         }.bind(this));
     };
+
+    Twitter4FxOS.prototype.OK = {};
+    Twitter4FxOS.prototype.NG = {};
 
     /**
      * OAuth承認を行います。
@@ -20,7 +25,7 @@ var Twitter4FxOS = (function(){
                 console.log("then");
                 resolve();
             }).catch(function(){
-                console.log(this);
+                console.log(this._network);
                 this._network.startOAuth().then(function(accessToken){
                     console.log("catch");
                     this._storage.setItem(accessToken);
@@ -30,6 +35,20 @@ var Twitter4FxOS = (function(){
                     reject();
                 });
             }.bind(this));
+        }.bind(this));
+    };
+
+    Twitter4FxOS.prototype.fetchAccessToken = function(pin){
+        return new Promise(function(resolve, reject) {
+            console.log(pin, this);
+
+            this._network.fetchAccessToken(pin).then(function (accessToken) {
+                this._storage.setItem(accessToken);
+                resolve();
+            }.bind(this)).catch(function () {
+                console.log("network error");
+                reject();
+            });
         }.bind(this));
     };
 
@@ -49,26 +68,24 @@ var Twitter4FxOS = (function(){
         this._accessToken = accessToken;
     };
 
+    Network.prototype._CONSUMER_KEY = "x1Z4hAckN1a5LTQKouGQ";
+    Network.prototype._CONSUMER_SECRET = "2FXyGxHDh856bZXTO5dhDvaFl3SqQp65EQZItT6k";
+
     Network.prototype.startOAuth = function(){
         return new Promise(function(resolve, reject){
-            const CONSUMER_KEY = "x1Z4hAckN1a5LTQKouGQ";
-            const CONSUMER_SECRET = "2FXyGxHDh856bZXTO5dhDvaFl3SqQp65EQZItT6k";
-
             var message = {
                 method: "GET",
                 action: "https://api.twitter.com/oauth/request_token",
                 parameters: {
                     oauth_signature_method: "HMAC-SHA1",
-                    oauth_consumer_key: CONSUMER_KEY
+                    oauth_consumer_key: this._CONSUMER_KEY
                 }
             };
 
             OAuth.setTimestampAndNonce(message);
             OAuth.SignatureMethod.sign(message, {
-                consumerSecret: CONSUMER_SECRET
+                consumerSecret: this._CONSUMER_SECRET
             });
-
-            console.log(this);
 
             this._$http({
                 url: OAuth.addToURL(message.action, message.parameters),
@@ -76,45 +93,43 @@ var Twitter4FxOS = (function(){
             }).success(function(data){
                 console.log(data);
                 var _ = data.match(/oauth_token=([^&]*)&oauth_token_secret=([^&]*)/);
-                const OAUTH_TOKEN = _[1];
-                const OAUTH_TOKEN_SECRET = _[2];
-                const URL = "https://api.twitter.com/oauth/authorize?oauth_token="+OAUTH_TOKEN;
+                this._OAUTH_TOKEN = _[1];
+                this._OAUTH_TOKEN_SECRET = _[2];
+                const URL = "https://api.twitter.com/oauth/authorize?oauth_token="+this._OAUTH_TOKEN;
                 console.log(URL);
                 window.open(URL, "_blank");
-/*
-                this._$http({
-                    url:URL
-                }).success(function(){
-                    console.log(arguments);
-                });*/
-
-                /*
-                var message = {
-                    method: "GET",
-                    action: "https://twitter.com/oauth/access_token",
-                    parameters: {
-                        oauth_signature_method: "HMAC-SHA1",
-                        oauth_consumer_key: CONSUMER_KEY,
-                        oauth_token: OAUTH_TOKEN,
-                        oauth_verifier: pin
-                    }
-                };
-                OAuth.setTimestampAndNonce(message);
-                OAuth.SignatureMethod.sign(message, {
-                    consumerSecret: CONSUMER_SECRET,
-                    tokenSecret: OAUTH_TOKEN_SECRET
-                });
-
-                this._$http({
-                    url: OAuth.addToURL(message.action, message.parameters),
-                    method: message.method
-                }).success(function(){
-                    console.log(arguments);
-                }).error(function(){
-                    console.log(arguments);
-                });
-                */
             }.bind(this)).error(function(){
+                console.log(arguments);
+                reject();
+            });
+        }.bind(this));
+    };
+
+    Network.prototype.fetchAccessToken = function(pin){
+        return new Promise(function(resolve, reject){
+            var message = {
+                method: "GET",
+                action: "https://twitter.com/oauth/access_token",
+                parameters: {
+                    oauth_signature_method: "HMAC-SHA1",
+                    oauth_consumer_key: this._CONSUMER_KEY,
+                    oauth_token: this._OAUTH_TOKEN,
+                    oauth_verifier: pin
+                }
+            };
+            OAuth.setTimestampAndNonce(message);
+            OAuth.SignatureMethod.sign(message, {
+                consumerSecret: this._CONSUMER_SECRET,
+                tokenSecret: this._OAUTH_TOKEN_SECRET
+            });
+
+            this._$http({
+                url: OAuth.addToURL(message.action, message.parameters),
+                method: message.method
+            }).success(function(){
+                console.log(arguments);
+                resolve();
+            }).error(function(){
                 console.log(arguments);
                 reject();
             });
